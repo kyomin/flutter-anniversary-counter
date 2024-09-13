22ec3dd5b9041c3c4806d0ae5af375ffc49ae4d2
jake.kyo <jake.kyo@kakaocorp.com>
2024-09-13 22:22:08 +0900

    디자인 작업하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink[100],
       body: SafeArea(
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
@@ -13,20 +14,48 @@ class HomeScreen extends StatelessWidget {
             children: [
               Expanded(
                 child: Container(
-                  color: Colors.red,
                   child: Column(
                     children: [
-                      Text('Anniversary Counter'),
-                      Text('우리들의 기념일'),
-                      IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-                      Text('D+1')
+                      Text(
+                        'Anniversary Counter',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'parisienne',
+                            fontSize: 40.0),
+                      ),
+                      Text(
+                        '우리들의 기념일',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'sunflower',
+                            fontSize: 20.0),
+                      ),
+                      Text(
+                        '2024-9-18',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'sunflower',
+                            fontSize: 15.0),
+                      ),
+                      IconButton(
+                          iconSize: 40.0,
+                          color: Colors.red,
+                          onPressed: () {},
+                          icon: Icon(Icons.favorite)),
+                      Text(
+                        'D+1',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'sunflower',
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w700),
+                      )
                     ],
                   ),
                 ),
               ),
               Expanded(
                 child: Container(
-                  color: Colors.blue,
                   child: Image.asset('asset/img/middle_image.png'),
                 ),
               )
