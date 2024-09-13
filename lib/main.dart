5a492dfb1b4f6f3bc0775ba60f73b2d014a4d172
jake.kyo <jake.kyo@kakaocorp.com>
2024-09-13 23:31:54 +0900

    테마 적용하여 스타일 코드 간결하게 하기

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:anniversary_counter/screen/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    theme: ThemeData(
+        fontFamily: 'sunflower',  // 전역에서 사용할 기본 폰트 설정
+        textTheme: TextTheme(
+            displayLarge: TextStyle(
+                color: Colors.white, fontSize: 40.0, fontFamily: 'parisienne'),
+            displayMedium: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.w700),
+            bodyLarge: TextStyle(color: Colors.white, fontSize: 20.0),
+            bodyMedium: TextStyle(color: Colors.white, fontSize: 15.0))),
     home: HomeScreen(),
   ));
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,57 +11,61 @@ class HomeScreen extends StatelessWidget {
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
           child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  child: Column(
-                    children: [
-                      Text(
-                        'Anniversary Counter',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'parisienne',
-                            fontSize: 40.0),
-                      ),
-                      Text(
-                        '우리들의 기념일',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'sunflower',
-                            fontSize: 20.0),
-                      ),
-                      Text(
-                        '2024-9-18',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'sunflower',
-                            fontSize: 15.0),
-                      ),
-                      IconButton(
-                          iconSize: 40.0,
-                          color: Colors.red,
-                          onPressed: () {},
-                          icon: Icon(Icons.favorite)),
-                      Text(
-                        'D+1',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'sunflower',
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.w700),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  child: Image.asset('asset/img/middle_image.png'),
-                ),
-              )
-            ],
+            children: [_Top(), _Bottom()],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _Top extends StatelessWidget {
+  const _Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Text(
+              'Anniversary Counter',
+              style: textTheme.displayLarge,
+            ),
+            Text(
+              '우리들의 기념일',
+              style: textTheme.bodyLarge,
+            ),
+            Text(
+              '2024-9-18',
+              style: textTheme.bodyMedium,
+            ),
+            IconButton(
+                iconSize: 40.0,
+                color: Colors.red,
+                onPressed: () {},
+                icon: Icon(Icons.favorite)),
+            Text(
+              'D+1',
+              style: textTheme.displayMedium,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset('asset/img/middle_image.png'),
       ),
     );
   }
