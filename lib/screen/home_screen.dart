7a22cbb4a826265f8022b72958ee3163427240e3
jake.kyo <jake.kyo@kakaocorp.com>
2024-09-14 17:22:21 +0900

    상태 상위로 올려 관리하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -12,23 +19,50 @@ class HomeScreen extends StatelessWidget {
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
           child: Column(
-            children: [_Top(), _Bottom()],
+            children: [
+              _Top(
+                selectedDate: selectedDate,
+                onPressed: onHeartPressed,
+              ),
+              _Bottom()
+            ],
           ),
         ),
       ),
     );
   }
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return Align(
+            alignment: Alignment.center,
+            child: Container(
+              color: Colors.white,
+              height: 300.0,
+              child: CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.date,
+                onDateTimeChanged: (DateTime date) {
+                  // setState에 의해 build가 다시 실행된다.
+                  setState(() {
+                    selectedDate = date;
+                  });
+                },
+                dateOrder: DatePickerDateOrder.ymd,
+              ),
+            ),
+          );
+        });
+  }
 }
 
-class _Top extends StatefulWidget {
-  const _Top({super.key});
+class _Top extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback? onPressed;
 
-  @override
-  State<_Top> createState() => _TopState();
-}
-
-class _TopState extends State<_Top> {
-  DateTime selectedDate = DateTime.now();
+  const _Top({required this.selectedDate, required this.onPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -36,55 +70,30 @@ class _TopState extends State<_Top> {
     final textTheme = Theme.of(context).textTheme;
 
     return Expanded(
-      child: Container(
-        child: Column(
-          children: [
-            Text(
-              'Anniversary Counter',
-              style: textTheme.displayLarge,
-            ),
-            Text(
-              '우리들의 기념일',
-              style: textTheme.bodyLarge,
-            ),
-            Text(
-              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-              style: textTheme.bodyMedium,
-            ),
-            IconButton(
-                iconSize: 40.0,
-                color: Colors.red,
-                onPressed: () {
-                  showCupertinoDialog(
-                      context: context,
-                      barrierDismissible: true,
-                      builder: (BuildContext context) {
-                        return Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            color: Colors.white,
-                            height: 300.0,
-                            child: CupertinoDatePicker(
-                              mode: CupertinoDatePickerMode.date,
-                              onDateTimeChanged: (DateTime date) {
-                                // setState에 의해 build가 다시 실행된다.
-                                setState(() {
-                                  selectedDate = date;
-                                });
-                              },
-                              dateOrder: DatePickerDateOrder.ymd,
-                            ),
-                          ),
-                        );
-                      });
-                },
-                icon: Icon(Icons.favorite)),
-            Text(
-              'D+${now.difference(selectedDate).inDays + 1}',
-              style: textTheme.displayMedium,
-            )
-          ],
-        ),
+      child: Column(
+        children: [
+          Text(
+            'Anniversary Counter',
+            style: textTheme.displayLarge,
+          ),
+          Text(
+            '우리들의 기념일',
+            style: textTheme.bodyLarge,
+          ),
+          Text(
+            '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}', // 부모의 state 접근을 위해 widget 사용하여 접근
+            style: textTheme.bodyMedium,
+          ),
+          IconButton(
+              iconSize: 40.0,
+              color: Colors.red,
+              onPressed: onPressed,
+              icon: Icon(Icons.favorite)),
+          Text(
+            'D+${now.difference(selectedDate).inDays + 1}',
+            style: textTheme.displayMedium,
+          )
+        ],
       ),
     );
   }
